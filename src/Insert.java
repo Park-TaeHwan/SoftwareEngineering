a6fd9f8ab402455f40ca6f569190b3914934de53
User <User@58.234.36.174>
2022-05-31 21:11:40 +0900

    1

--- src/Insert.java
+++ src/Insert.java
@@ -85,37 +85,49 @@ public class Insert {
 		  }catch(NullPointerException e){
 		   System.out.println("Err : "+e);
 		  } //end try-catch
+		  updateFood();
 		  
-		  Scanner keyboard = new Scanner(System.in);
-			System.out.println("Enter food product ");
-			String name = keyboard.nextLine();
-			
-			stockReadymade prod = ps.searchProd(name);
-			int updateNum = pv.updateNum(); 
-			if(prod != null) {
-				if(updateNum != -1) {
-					//정상적인 변경 값이 입력되었을 경우
-					//서비스 클래스의 정보 수정 메소드 사용
-					boolean result = ps.updateProd(name, updateNum);
-					if(result) {
-						System.out.println("Update success ");
-					}
-					else {
-						//수정 실패 메시지 출력
-						System.out.println("Update failed ");
-					}
+	 } //end getTxt()
+	 
+	 private void updateFood() {
+		 ArrayList<stockReadymade> pList = ps.getStockList();
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter food product ");
+		String name = keyboard.nextLine();
+		
+		stockReadymade prod = ps.searchProd(name);
+		int updateNum = pv.updateNum(); 
+		if(prod != null) {
+			if(updateNum != -1) {
+				//정상적인 변경 값이 입력되었을 경우
+				//서비스 클래스의 정보 수정 메소드 사용
+				boolean result = ps.updateProd(name, updateNum);
+				if(result) {
+					System.out.println("Update success ");
 				}
 				else {
-					//엔터가 입력되었을 경우. 메시지 출력
-					System.out.println("wrong ");
+					//수정 실패 메시지 출력
+					System.out.println("Update failed ");
 				}
 			}
 			else {
-				System.out.println("none data");
+				//엔터가 입력되었을 경우. 메시지 출력
+				System.out.println("wrong ");
 			}
-	 } //end getTxt()
+		}
+		else {
+			System.out.println("none data");
+		}
+		
+		for(stockReadymade s : pList) {
+			 System.out.println(s);
+		}
+	}
 	 
-	 private void printProduct() {
+		
+
+	private void printProduct() {
 		 stockReadyMadeService ps = new stockReadyMadeService();
 			ps.startPs();
 			ArrayList<stockReadymade> pList = ps.getStockList();
